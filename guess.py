8e7963233dbe6f51e544619c10a885af18e2dffa
Rafeal Chen <winduanckc@gmail.com>
2019-01-27 22:14:53 +0800

    level up

--- guess.py
+++ guess.py
@@ -1,8 +1,14 @@
 import random
 
-r = random.randint(1, 100)
+x = input('請輸入範圍 (底標)')
+y = input('請輸入範圍 (高標)')
+x = int(x)
+y = int(y)
+r = random.randint(x, y)
+count = 0
 
 while True:
+	count += 1 ## 快寫法 for count = count + 1
 	test = input("數字猜猜看: ")
 	test = int(test)
 	
@@ -13,5 +19,6 @@ while True:
 		print('比答案大')
 	elif test < r:
 		print('比答案小')
+	print('這是你猜的第',count, '次!')
 
 
